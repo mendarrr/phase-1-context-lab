5d44595be28a10c520358fc1d6e5f091da98504c
mendarrr <sarahabby541@gmail.com>
2024-04-21 10:45:30 +0300

    Completed lab

--- index.js
+++ index.js
@@ -1,4 +1,70 @@
 /* Your Code Here */
+// the createEmployment function => To get the employee information
+function createEmployeeRecord(employeeDetails) {
+  // The destructuring assignment to assign variables
+  const [firstName, familyName, title, payPerHour] = employeeDetails;
+  return {
+    firstName,
+    familyName,
+    title,
+    payPerHour,
+    timeInEvents: [],
+    timeOutEvents: [],
+  };
+}
+
+// Processes an array of employee records(arrays in arrays kinda)
+function createEmployeeRecords(employeeDetailsArray) {
+  // An array of objects
+  return employeeDetailsArray.map(createEmployeeRecord);
+}
+
+// Adds an object with three keys: type, hour and date
+let createTimeInEvent = function(dateStamp){
+  let [date, hour] = dateStamp.split(' ')
+ // Create the timeInEvent object
+  this.timeInEvents.push({
+      type: "TimeIn",
+      hour: parseInt(hour, 10),
+      date,
+  })
+
+  return this
+}
+
+// Adds an object with three keys: type, hour and date
+let createTimeOutEvent = function(dateStamp){
+  let [date, hour] = dateStamp.split(' ')
+ // Create the timeOutEvent object
+  this.timeOutEvents.push({
+      type: "TimeOut",
+      hour: parseInt(hour, 10),
+      date,
+  })
+
+  return this
+}
+
+//Calculate the number of hours worked
+let hoursWorkedOnDate = function(soughtDate){
+let inEvent = this.timeInEvents.find(function(e){
+    return e.date === soughtDate
+})
+
+let outEvent = this.timeOutEvents.find(function(e){
+    return e.date === soughtDate
+})
+
+return (outEvent.hour - inEvent.hour) / 100
+}
+
+
+// Adds together all the wages collected hence returns a single value
+let wagesEarnedOnDate = function(dateSought){
+let rawWage = hoursWorkedOnDate.call(this, dateSought)
+    * this.payPerHour
+return parseFloat(rawWage.toString())
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -20,4 +86,17 @@ const allWagesFor = function () {
 
     return payable
 }
+
+let findEmployeeByFirstName = function(srcArray, firstName) {
+  return srcArray.find(function(rec){
+    return rec.firstName === firstName
+  })
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + allWagesFor.call(rec)
+    }, 0)
+}
+
 
